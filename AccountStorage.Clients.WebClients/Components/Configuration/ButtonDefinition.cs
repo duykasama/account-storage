b1c213f6d3b43fe8b4c5bb72ad4baf89a917f2bc
duykasama <duykasama@gmail.com>
2023-08-11 00:31:36 +0700

    display message before deleting account

---
+++ AccountStorage.Clients.WebClients/Components/Configuration/ButtonDefinition.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AccountStorage.Clients.WebClients.Components.Configuration
+{
+    public class ButtonDefinition
+    {
+        public string Name { get; set; }
+
+        public Action HandleOnclick { get; set; }
+
+        public string CssClass { get; set; }
+    }
+}
---
+++ AccountStorage.Clients.WebClients/Components/Configuration/PagingConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace AccountStorage.Clients.WebClients.Components.Configuration
+{
+    public class PagingConfig
+    {
+        public bool Enabled { get; set; }
+
+        public int PageSize { get; set; }
+
+        public bool UseCustomPager { get; set; }
+
+        public int NumberOfItemsToSkip(int pageNumber) => Enabled ? (pageNumber - 1) * PageSize : 0;
+
+        public int NumberOfItemsToTake(int totalItemsCount) => Enabled ? PageSize : totalItemsCount;
+
+        public int PreviousPageNumber(int currentPageNumber) => (currentPageNumber > 1) ? currentPageNumber - 1 : 1;
+
+        public int NextPageNumber(int currentPageNumber, int totalItemsCount) => (currentPageNumber < LastPage(totalItemsCount)) ? currentPageNumber + 1 : currentPageNumber;
+
+        public int LastPage(int totalItemsCount) => int.Parse(Math.Ceiling((double)totalItemsCount / (double)PageSize).ToString());
+
+    }
+}
